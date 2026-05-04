0a4887f1e60500c0ceff6fb0e798711ca6d82abd
Kilian Huegler <kilianhuegler@icloud.com>
2026-05-04 14:11:40 +0300

    Refactoring: injected today parameter

--- src/testable1.mjs
+++ src/testable1.mjs
@@ -4,13 +4,13 @@
 
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+export function daysUntilChristmas(today) {
+  const now = today;
+  const todayDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
-  if (today.getTime() > christmasDay.getTime()) {
+  if (todayDate.getTime() > christmasDay.getTime()) {
     christmasDay.setFullYear(new Date().getFullYear() + 1);
   }
-  const diffMillis = christmasDay.getTime() - today.getTime();
+  const diffMillis = christmasDay.getTime() - todayDate.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
