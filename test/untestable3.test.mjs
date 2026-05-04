06fb5ce91779c08984e1a949ce8c8d7862266ee9
Kilian Huegler <kilianhuegler@icloud.com>
2026-05-04 18:57:40 +0300

    testable3.mjs first test

--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -1,6 +1,6 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { parsePeopleCsv } from "../src/untestable3.mjs";
+import { parsePeopleCsv } from "../src/testable3.mjs";
 
 // example input:
 // Loid,Forger,,Male
@@ -8,10 +8,8 @@ import { parsePeopleCsv } from "../src/untestable3.mjs";
 // Yor,Forger,27,Female
 
 describe("Untestable 3: CSV file parsing", () => {
-  test("todo", async () => {
-    // TODO: write proper tests
-    try {
-      expect(await parsePeopleCsv("people.csv")).to.deep.equal([]);
-    } catch (e) {}
+  test("parse a row with fields", () => {
+    const csv = "Yor,Forger,27,Female";
+    expect(parsePeopleCsv(csv)).to.deep.equal([{ firstName: "Yor", lastName: "Forger", age: 27, gender: "f" }]);
   });
 });
