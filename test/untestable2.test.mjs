fc190f75b07487e16fa3340f2af7c16c9cf22596
Kilian Huegler <kilianhuegler@icloud.com>
2026-05-04 17:08:42 +0300

    Use copy in untestable2.mjs for testing

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,6 +1,6 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { diceHandValue } from "../src/untestable2.mjs";
+import { diceHandValue } from "../src/testable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
   test("todo", () => {
