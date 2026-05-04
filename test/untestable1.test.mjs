48aad42447764679c7dcb25e70ee793a341edae4
Kilian Huegler <kilianhuegler@icloud.com>
2026-05-04 14:08:21 +0300

    Added testable1 test

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -1,10 +1,10 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/untestable1.mjs";
+import { daysUntilChristmas } from "../src/testable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("one day before Christmas", () => {
+    const today = new Date(2024, 11, 24);
+    expect(daysUntilChristmas(today)).to.equal(1);
   });
 });
