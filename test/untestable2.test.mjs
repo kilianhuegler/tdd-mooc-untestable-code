f0b65b2537bc073c14e6f670f559fceea26cb7c9
Kilian Huegler <kilianhuegler@icloud.com>
2026-05-04 18:38:29 +0300

    testable2.mjs high die test

--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -7,4 +7,13 @@ describe("Untestable 2: a dice game", () => {
     const stubRandom = () => 0.5;
     expect(diceHandValue(stubRandom)).to.equal(104);
   });
+
+  test("high die", () => {
+    let callCount = 0;
+    const stubRandom = () => {
+      callCount++;
+      return callCount === 1 ? 0.0 : 0.5;
+    };
+    expect(diceHandValue(stubRandom)).to.equal(4);
+  })
 });
