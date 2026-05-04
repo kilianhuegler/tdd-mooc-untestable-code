6ba8825f2e34548e3cf1ab2f41c59336b475ecfc
Kilian Huegler <kilianhuegler@icloud.com>
2026-05-04 19:08:01 +0300

    testable3.mjs added more tests

--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -12,4 +12,22 @@ describe("Untestable 3: CSV file parsing", () => {
     const csv = "Yor,Forger,27,Female";
     expect(parsePeopleCsv(csv)).to.deep.equal([{ firstName: "Yor", lastName: "Forger", age: 27, gender: "f" }]);
   });
+
+  test("parse a row with a missing field", () => {
+    const csv = "Yor,Forger,,Female";
+    expect(parsePeopleCsv(csv)).to.deep.equal([{ firstName: "Yor", lastName: "Forger", gender: "f" }]);
+  })
+
+  test("parse multiple rows", () => {
+    const csv = "Yor,Forger,27,Female\nAnya,Forger,6,Female";
+    expect(parsePeopleCsv(csv)).to.deep.equal([
+      { firstName: "Yor", lastName: "Forger", age: 27, gender: "f" },
+      { firstName: "Anya", lastName: "Forger", age: 6, gender: "f" },
+    ]);
+  });
+
+  test("skips empty lines", () => {
+    const csv = "Yor,Forger,27,Female\n\nAnya,Forger,6,Female";
+    expect(parsePeopleCsv(csv)).to.have.lengthOf(2);
+  });
 });
